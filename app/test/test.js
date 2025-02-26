9a89ecb94f9f77a0816d7622d998d2130a3dba3a
giorgi kapanadze <georgegiorgika@gmail.com>
2025-02-26 11:44:49 +0000

    improove request making cound depend on if rework cost is empty

---
+++ app/test/test.js
@@ -0,0 +1,28 @@
+const gio = {
+  nia: [
+    {
+      eka: "nadiri",
+      gogi: "debili",
+      gao: "1",
+      lasha: "narmalna",
+    },
+    {
+      eka: "nadiriiiii",
+      gogi: "debili",
+      gao: "2",
+      lasha: "narmalna",
+    },
+    {
+      eka: "nadiri",
+      gogi: "debili",
+      gao: "4",
+      lasha: "narmalna",
+    },
+    {
+      eka: "nadiriiiii",
+      gogi: "debili",
+      gao: "2",
+      lasha: "narmalna",
+    },
+  ],
+};
